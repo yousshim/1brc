27aac7392cdd5fb86658f8dce9519e941d6fb920
yousshim <yousef98ibrahim@gmail.com>
2026-03-01 11:27:45 +0200

    reduce map reads

--- calculate.go
+++ calculate.go
@@ -24,26 +24,24 @@ func (stat stationStat) String() string {
 
 func Calculate(r io.Reader, w io.Writer) {
 	s := bufio.NewScanner(r)
-	stats := make(map[string]stationStat)
+	stats := make(map[string]*stationStat)
 	for s.Scan() {
 		line := s.Text()
 		parts := strings.Split(line, ";")
 		name := parts[0]
 		temp, _ := strconv.ParseFloat(parts[1], 64)
-		if _, ok := stats[name]; !ok {
-			stats[name] = stationStat{
+		if stat, ok := stats[name]; ok {
+			stat.min = min(stat.min, temp)
+			stat.max = max(stat.max, temp)
+			stat.sum += temp
+			stat.cnt++
+		} else {
+			stats[name] = &stationStat{
 				min: temp,
 				max: temp,
 				sum: temp,
 				cnt: 1,
 			}
-		} else {
-			stat := stats[name]
-			stat.min = min(stat.min, temp)
-			stat.max = max(stat.max, temp)
-			stat.sum += temp
-			stat.cnt++
-			stats[name] = stat
 		}
 	}
 	for _, key := range slices.Sorted(maps.Keys(stats)) {
